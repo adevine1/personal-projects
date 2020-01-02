c6b5ecb7f7396d92df5c2c200dc537f0bc672efa
Aaron Devine <a.f.devine@gmail.com>
2020-01-01 22:32:52 -0800

    added jquery code

---
+++ petals-around-the-rose/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log("document ready");
+
+  $("#rollbutton").click(function() {
+    console.log("button pressed");
+    let diceArray =[];
+    let t = 0;
+    for (let x=0; x<6; x++) {
+      let randNum = (Math.floor(Math.random ()*6) + 1);
+      diceArray.push (randNum);
+      if (randNum==3)
+        t += 2;
+      if (randNum==5)
+        t += 4;
+    }
+    console.log (diceArray);
+    console.log (t);
+   });
+});
